b919fc49c1a3afd18774580ff9b8dee788cd0b79
Andrew Lee Jackson <88171965+Andrew-L-Jackson@users.noreply.github.com>
2021-08-11 19:58:33 -0500

    Update Median.py

--- Median.py
+++ Median.py
@@ -5,6 +5,6 @@ test_list.sort()
 mid = len(test_list) // 2
 median = (test_list[mid] + test_list[~mid]) / 2
   
-print("Median of list is : " + str(res))
+print("Median of list is : " + str(median))
 
 
